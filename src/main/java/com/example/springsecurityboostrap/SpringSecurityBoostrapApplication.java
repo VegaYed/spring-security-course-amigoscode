27602c9b903820bde77a7e97eabe71528497ba15
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-09-13 22:52:47 -0300

    Seccion uno.

---
+++ src/main/java/com/example/springsecurityboostrap/SpringSecurityBoostrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityboostrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBoostrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBoostrapApplication.class, args);
+	}
+
+}
