25ef98478a22f1d97a3f744a5bb7569272042feb
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-09-16 01:21:40 -0300

    configuracion de permisos en base a permisos mediante annotations en los metodos del controller

--- src/main/java/com/example/springsecurityboostrap/student/StudentManagementController.java
+++ src/main/java/com/example/springsecurityboostrap/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityboostrap.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,26 +17,31 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAll(){
         return STUDENT_LIST;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public Student addStudent(@RequestBody Student student){
         return student;
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public Integer deleteStudent(@PathVariable Integer id){
         return id;
     }
 
     @PutMapping("{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public Student updateStudent(@PathVariable Integer id, @RequestBody Student student){
         return student;
     }
 
     @GetMapping("{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public Student getStudent(@PathVariable Integer id){
         return STUDENT_LIST.stream()
                 .filter(student -> id.equals(student.getId()))
